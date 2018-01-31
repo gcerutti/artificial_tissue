e6255f226247b37689dd1de9ccfd8f377c54d6e5
Hadrien Oliveri <hadrien.oliveri@inria.fr>
2018-01-31 15:00:11 +0100

    Added: script to generate cvt image from mesh surface. Not tested

---
+++ src/vplants/artificial_tissue/surface_to_tissue/mesh2cvt.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+# -*- python -*-
+#
+#       Artificial Tissue
+#
+#       Copyright 2017-2018 INRIA
+#
+#       File author(s): Hadrien Oliveri <hadrien.oliveri@inria.fr>
+#
+#       File contributor(s): Hadrien Oliveri <hadrien.oliveri@inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+###############################################################################
+
+import argparse
+import os
+from utils import *
+from openalea.image.serial.all import imread, imsave
+from openalea.mesh.property_topomesh_io import read_ply_property_topomesh
+
+
+def mesh_to_cvt_image(input, output='.', voxelsize=(.5, .5, .5), max_step=1e9, nbcells=100, method="lloyd",
+                      verbose=False, debug=False, save=False):
+    """
+
+    :param input:
+    :param output:
+    :param voxelsize:
+    :param method:
+    :param verbose:
+    :param debug:
+    :return:
+    """
+
+    assert os.path.exists(input), "Input file: " + input + " does not exist."
+    assert method in ["lloyd", "mcqueen"], "Wrong method."
+
+    if output is not None and save and not os.path.exists(output):
+        os.makedirs(output)
+
+    logging.getLogger().setLevel(logging.INFO if verbose else logging.DEBUG if debug else logging.ERROR)
+    logging.info("Generating image from mesh.")
+
+    # Loading mesh
+    logging.info("Loading mesh")
+    mesh = read_ply_property_topomesh(input)
+
+    # Binarized image
+    logging.info("Rasterizing mesh to binary image.")
+    mask = topomesh_to_binary_image(mesh=mesh, voxelsize=voxelsize, verbose=verbose, debug=debug)
+    if save and output is not None:
+        logging.info("Saving image to " + output)
+        bin_path = os.path.join(output, "bin.inr")
+        imsave(bin_path, mask)
+    from itertools import product
+    points = filter(lambda p: mask[p], list(product(range(mask.shape[0]), range(mask.shape[1]), range(mask.shape[2]))))
+
+    # Random seeds
+    logging.info("Choosing initial seeds.")
+    density = None  # TODO
+    seeds, labels, seed_img = random_seeds(mask, nb_seeds=nbcells, density=density, points=points,
+                                           verbose=verbose, debug=debug)
+    if save and output is not None:
+        logging.info("Saving image to " + output)
+        seed_path = os.path.join(output, "seeds.inr")
+        imsave(seed_path, seed_img)
+
+    # Initial Voronoi partioning
+    logging.info("Initializing Voronoi diagram.")
+    voronoi_img = voronoi(seeds, labels, mask=mask, points=points, verbose=verbose, debug=debug)
+    if save and output is not None:
+        logging.info("Saving image to " + output)
+        voronoi_path = os.path.join(output, "seeds.inr")
+        imsave(voronoi_path, voronoi_img)
+
+    # CVT
+    logging.info("Computing CENTROIDAL VORONOI TESSELLATION")
+    centroid_img, cvt_img = cvt(mask=mask, seeds=seeds, labels=labels, res_path=os.path.join(output, "mesh_to_cvt_image.inr"),
+                            steps=max_step, points=points, method=method, voronoi_img=voronoi_img, save=save, verbose=verbose,
+                                debug=debug)
+
+    return cvt_img
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help='path to input mesh', required=True)
+    parser.add_argument('-o', '--output', help='path to output files directory', default="./output")
+    parser.add_argument('-n', '--nbcells', help='Number of cells', default=100)
+    parser.add_argument('--step', help='Maximal number of steps for CVT', default=1e9)
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='verbose')
+    parser.add_argument('-d', '--debug', default=False, action='store_true', help='debug')
+    parser.add_argument('-s', '--save', default=False, action='store_true', help='save output image to file')
+    parser.add_argument('--voxelsize', help='Voxel size', default=[.025, .025, .025], nargs=3)
+    parser.add_argument('-m', '--method', help='Method for CVT [\'lloyd\', \'mcqueen\']', default='lloyd')
+
+    args = parser.parse_args()
+
+    mesh_to_cvt_image(input=args.input, output=args.output, method=args.method, verbose=args.verbose, debug=args.debug,
+        save=args.save, voxelsize=args.voxelsize, nbcells=args.nbcells, max_step=args.step)
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/vplants/artificial_tissue/surface_to_tissue/utils.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+# -*- python -*-
+#
+#       Artificial Tissue
+#
+#       Copyright 2017-2018 INRIA
+#
+#       File author(s): Hadrien Oliveri <hadrien.oliveri@inria.fr>
+#
+#       File contributor(s): Hadrien Oliveri <hadrien.oliveri@inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+###############################################################################
+
+import numpy as np
+import logging
+from itertools import product
+from openalea.image.spatial_image import SpatialImage, empty_image_like
+from scipy.spatial import cKDTree
+
+
+def topomesh_to_binary_image(mesh, voxelsize=(.5, .5, .5), verbose=False, debug=False):
+    """
+
+    :param polydata:
+    :param voxelsize:
+    :param bounds:
+    :param verbose:
+    :param debug:
+    :return:
+    """
+
+    from vtk import vtkImageData, vtkPolyDataToImageStencil, vtkImageStencil, VTK_UNSIGNED_CHAR
+    from vtk.util.numpy_support import vtk_to_numpy
+    from openalea.cellcomplex.property_topomesh.triangular_mesh import topomesh_to_triangular_mesh
+
+    logging.getLogger().setLevel(logging.INFO if verbose else logging.DEBUG if debug else logging.ERROR)
+
+    mesh, matching = topomesh_to_triangular_mesh(mesh, degree=3, coef=1.0, property_name=None,
+                                                 property_degree=None)
+    polydata = mesh._repr_vtk_()
+    bounds = polydata.GetBounds()
+
+    # Initializing image
+    logging.info("Initializing image")
+    white_image = vtkImageData()
+    white_image.SetSpacing(voxelsize)
+    dim = [int(np.ceil((bounds[i * 2 + 1] - bounds[i * 2]) / voxelsize[i])) for i in range(3)]
+
+    # FIXME: does not work if dimensions are different
+    dim = [np.amax(dim)] * 3
+
+    logging.info("Image dimension: " + str(dim))
+    white_image.SetDimensions(dim)
+    white_image.SetExtent(-1, dim[0] - 1, -1, dim[1] - 1, -1, dim[2] - 1)
+    origin = [bounds[i * 2] for i in range(3)]
+    # origin = [.5 *(bounds[i * 2]+bounds[i * 2 + 1]) for i in range(3)]
+
+    white_image.SetOrigin(origin)
+    logging.info("Image origin: " + str(white_image.GetOrigin()))
+    white_image.AllocateScalars(VTK_UNSIGNED_CHAR, 1)
+
+    count = white_image.GetNumberOfPoints()
+    for i in range(count):
+        white_image.GetPointData().GetScalars().SetTuple1(i, 255)
+
+    pol2stenc = vtkPolyDataToImageStencil()
+    pol2stenc.SetInputData(polydata)
+    pol2stenc.SetOutputOrigin(origin)
+    pol2stenc.SetOutputSpacing(voxelsize)
+    pol2stenc.SetOutputWholeExtent(white_image.GetExtent())
+
+    imgstenc = vtkImageStencil()
+    imgstenc.SetInputData(white_image)
+    imgstenc.SetStencilConnection(pol2stenc.GetOutputPort())
+    imgstenc.ReverseStencilOff()
+    imgstenc.SetBackgroundValue(0)
+    imgstenc.Update()
+
+    im = imgstenc.GetOutput()
+    rows, cols, depth = im.GetDimensions()
+    sc = im.GetPointData().GetScalars()
+    a = vtk_to_numpy(sc)
+    a = a.reshape(rows, cols, depth)
+
+    assert a.shape == im.GetDimensions()
+
+    return SpatialImage(a, vdim=1, dtype=np.uint16)
+
+
+def random_seeds(img, nb_seeds=100, density=None, points=None, replace=False, verbose=False, debug=False):
+    """
+
+    :param img:
+    :param nb_seeds:
+    :param density:
+    :param points:
+    :param replace:
+    :param verbose:
+    :param debug:
+    :return:
+    """
+    assert nb_seeds > 0, "Illegal number of seeds."
+    logging.getLogger().setLevel(logging.INFO if verbose else logging.DEBUG if debug else logging.ERROR)
+
+    if points is None:
+        assert img is not None
+        points = filter(lambda p: img[p],
+                        list(product(range(img.shape[0]), range(img.shape[1]), range(img.shape[2]))))
+
+    vol = len(points)
+    logging.info("Finding " + str(nb_seeds) + " random seeds in " + str(vol) + " voxels.")
+
+    res_img = empty_image_like(img)
+    logging.info("Uniform density of seeds." if density is None else "Non-uniform density of seeds")
+    seeds = np.asarray(points)[np.random.choice(len(points), nb_seeds, replace=replace, p=density)]
+
+    labels = np.uint16(np.arange(nb_seeds) + 1)
+    res_img[tuple(np.asarray(seeds).T)] = labels
+
+    return seeds, labels, res_img
+
+
+def voronoi(seeds, labels, mask=None, points=None, verbose=False, debug=False):
+    """
+
+    :param seeds:
+    :param labels:
+    :param mask:
+    :param points:
+    :param verbose:
+    :param debug:
+    :return:
+    """
+    logging.getLogger().setLevel(logging.INFO if verbose else logging.DEBUG if debug else logging.ERROR)
+
+    if points is None:
+        assert mask is not None and mask.shape == mask.shape
+        points = filter(lambda p: mask[p],
+                        list(product(range(mask.shape[0]), range(mask.shape[1]), range(mask.shape[2]))))
+
+    # KD tree for nearest point research
+    nearest = cKDTree(data=seeds).query(points)[1]
+    res_img = empty_image_like(mask)
+    res_img[tuple(np.transpose(points))] = labels[nearest]
+
+    return res_img
+
+
+def centroids(img, labels, bg=0):
+    """
+
+    :param img:
+    :param labels:
+    :param bg:
+    :return:
+    """
+    label_colors = {}
+    for x in range(len(img)):
+        for y in range(len(img[x])):
+            for z in range(len(img[x, y])):
+                if img[x, y, z] != bg:
+                    if img[x, y, z] not in label_colors:
+                        label_colors[img[x, y, z]] = [[x, y, z]]
+                    else:
+                        label_colors[img[x, y, z]] += [[x, y, z]]
+
+    centroids = {}
+    for l in labels:
+        pos = label_colors[l]
+        centroids[l] = np.sum(pos, axis=0) / float(len(pos))
+
+    return np.asarray(centroids.values())
+
+
+def cvt(mask, seeds, labels, steps=1e3, voronoi_img=None, res_path=None, points=None,
+        method="lloyd", intermediary_step=5e6, verbose=False, debug=False, save=False):
+    """
+    Centroidal Voronoi tesselation
+
+    :param mask:
+    :param seeds:
+    :param labels:
+    :param steps:
+    :param voronoi_img:
+    :param res_path:
+    :param points:
+    :param method:
+    :param intermediary_step:
+    :return:
+    """
+
+    from openalea.image.serial.all import imsave
+    logging.getLogger().setLevel(logging.INFO if verbose else logging.DEBUG if debug else logging.ERROR)
+
+    assert method in ['lloyd', 'macqueen']
+
+    logging.info("Centroidal Voronoi tessellation.")
+    logging.info("Method: " + method)
+
+    assert steps >= 0, "Illegal number of steps"
+
+    residual = lambda s1, s2: max(np.linalg.norm(np.asarray(s1) - np.asarray(s2), axis=0))
+    seeds = np.double(seeds)
+
+    if method == "lloyd":
+
+        assert voronoi_img is not None
+        prev_seeds = seeds
+        s = 0
+        while s < steps:
+            s += 1
+            seeds = centroids(voronoi_img, labels)
+            res = residual(seeds, prev_seeds)
+            logging.info("CVT step " + str(s) + " --> Residual:" + str(res))
+            voronoi_img = voronoi(seeds, labels, mask, points, verbose=verbose, debug=debug)
+            prev_seeds = seeds
+            if res_path is not None and save:
+                imsave(res_path, voronoi_img)
+            if np.isclose(0, res):
+                logging.info("Exiting")
+                break
+
+    elif method == "macqueen":
+
+        # FIXME: does not converge
+
+        points = np.double(points)
+        nb_seeds = len(seeds)
+        memory = dict(zip(range(nb_seeds), [0] * nb_seeds))
+        recompute_voronoi = True
+        s = 0
+        while s < steps:
+            if not s % (intermediary_step / 5):
+                logging.info("CVT step " + str(s))
+            s += 1
+            random_point = points[np.random.choice(len(points))]
+            closest = np.argmin(np.linalg.norm(seeds - random_point, axis=1))
+            memory[closest] += 1
+            seeds[closest] = (random_point + memory[closest] * np.asarray(seeds[closest])) / (memory[closest] + 1)
+            if not s % intermediary_step and s:
+                voronoi_img = voronoi(seeds, labels, mask, np.uint16(points))
+                c = centroids(voronoi_img, labels)
+                res = residual(seeds, c)
+                logging.info("CVT step " + str(s) + " --> Residual:" + str(res))
+
+                if res_path is not None and save:
+                    imsave(res_path, voronoi_img)
+                if np.isclose(0, res):
+                    recompute_voronoi = False
+                    break
+
+        if recompute_voronoi:
+            voronoi_img = voronoi(seeds, labels, mask, points)
+            if res_path is not None:
+                imsave(res_path, voronoi_img)
+
+    return seeds, voronoi_img
